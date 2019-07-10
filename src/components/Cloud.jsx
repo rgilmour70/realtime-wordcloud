8f3092544522ca1f2f9f9ed664cc3b90a1c8148d
Ron Gilmour <rgilmour70@gmail.com>
2019-07-10 12:43:45 -0400

    made words stop splitting on refresh

---
+++ src/components/Cloud.jsx
@@ -0,0 +1,26 @@
+// eslint-disable-next-line
+import React, { Component } from 'react';
+import ReactWordCloud from 'react-wordcloud';
+import { tallyWords } from '../utils/TallyWords';
+
+// This component originally used map() to 
+// display multiple Comment components.
+const Cloud = (props) => {
+
+  const wordObjs = tallyWords(props.words);
+
+  return (
+    <ReactWordCloud
+      options={{
+        rotations: 0,
+        colors: ['#4682b4', '#ff8c42', '#ff3c38', '#a23e48'],
+        fontSizes: [16, 36],
+        fontFamily: 'sans-serif',
+      }}
+      words={wordObjs} 
+    />
+  );
+
+}
+
+export default Cloud;
